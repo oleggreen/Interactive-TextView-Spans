f87795e3ad5fa9d66b9b1a988b1c98fb970a2618
Oleg Green <deseagle@bigmir.net>
2013-07-21 23:38:49 +0300

    correcting touch events for interactive spans

--- src/com/sundesign/InteractiveSpanActivity.java
+++ src/com/sundesign/InteractiveSpanActivity.java
@@ -25,6 +25,11 @@ import com.sundesign.text.style.RelativeInteractiveImageSpan;
  */
 public class InteractiveSpanActivity extends Activity {
 
+    public static final String TEXT1 = "Android is a Linux-based operating system designed primarily "
+            + "for touchscreen mobile devices such as smartphones and tablet computers";
+    public static final String TEXT2 = "Here's a link to a developers website: " +
+            "link http://developer.android.com";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -32,21 +37,21 @@ public class InteractiveSpanActivity extends Activity {
         setContentView(R.layout.main);
 
         TextView textView = (TextView) findViewById(R.id.TextView);
-        textView.setText("Android is a Linux-based operating system designed primarily "
-                + "for touchscreen mobile devices such as smartphones and tablet computers");
+        textView.setText(TEXT1);
         Spannable spannable = (Spannable) textView.getText();
 
         StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
         spannable.setSpan(boldSpan, 13, 24, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
 
         TextView textView2 = (TextView) findViewById(R.id.TextView2);
-        SpannableStringBuilder ssb = new SpannableStringBuilder("Here's a link to a developers website: " +
-                "link http://developer.android.com");
+        SpannableStringBuilder ssb = new SpannableStringBuilder(TEXT2 + TEXT1 + TEXT1 + TEXT1);
 
         InteractiveImageSpan imageSpan =
                 new RelativeInteractiveImageSpan(getStateListDrawable(), 2.4f, 5);
         InteractiveImageSpan imageSpan2 =
                 new FixedInteractiveImageSpan(getStateListDrawable(), 150, 30);
+        InteractiveImageSpan imageSpan3 =
+                new RelativeInteractiveImageSpan(getStateListDrawable(), 1.4f, 5);
 
         imageSpan.setOnClickListener(new InteractiveImageSpan.OnClickListener() {
 
@@ -66,9 +71,19 @@ public class InteractiveSpanActivity extends Activity {
                 Log.d("test", "span2 clicked");
             }
         });
+        imageSpan3.setOnClickListener(new InteractiveImageSpan.OnClickListener() {
+
+            @Override
+            public void onClick() {
+                Toast.makeText(getApplicationContext(),
+                        "hello from click method(span3)", Toast.LENGTH_SHORT).show();
+                Log.d("test", "span3 clicked");
+            }
+        });
 
         ssb.setSpan(imageSpan, 16, 17, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         ssb.setSpan(imageSpan2, 37, 38, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.setSpan(imageSpan3, 437, 438, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         URLSpan urlSpan = new URLSpan("http://developer.android.com") {
 
             @Override
--- src/com/sundesign/text/method/InteractiveSpanMovementMethod.java
+++ src/com/sundesign/text/method/InteractiveSpanMovementMethod.java
@@ -29,197 +29,91 @@ public class InteractiveSpanMovementMethod extends LinkMovementMethod {
     public boolean onTouchEvent(TextView widget, Spannable buffer,
                                 MotionEvent event) {
 
-        Log.d("test" , "onTouchEvent(), touch event = " + MotionEventHelper.actionToString(event.getAction())
-                + ", coords = [" + event.getX() + ", " + event.getY() + "]");
-
         int action = event.getAction();
 
-        if (action == MotionEvent.ACTION_UP ||
-                action == MotionEvent.ACTION_DOWN) {
-            int x = (int) event.getX();
-            int y = (int) event.getY();
+        int x = (int) event.getX();
+        int y = (int) event.getY();
 
-            x -= widget.getTotalPaddingLeft();
-            y -= widget.getTotalPaddingTop();
+        x -= widget.getTotalPaddingLeft();
+        y -= widget.getTotalPaddingTop();
 
-            x += widget.getScrollX();
-            y += widget.getScrollY();
+        x += widget.getScrollX();
+        y += widget.getScrollY();
 
-            Layout layout = widget.getLayout();
-            int line = layout.getLineForVertical(y);
-            int off = layout.getOffsetForHorizontal(line, x);
+        Layout layout = widget.getLayout();
+        int lineCount = layout.getLineCount();
+        int line = layout.getLineForVertical(y);
+        int textTopBound = layout.getLineTop(0);
+        int textBottomBound = layout.getLineBottom(lineCount - 1);
 
-            TouchableSpan[] link = buffer.getSpans(off, off, TouchableSpan.class);
+        int off = layout.getOffsetForHorizontal(line, x);
 
-            if (link.length != 0) {
-                if (action == MotionEvent.ACTION_UP) {
+        Log.d("motion", "onTouchEvent(), line = " + line
+                + ", offset = " + off);
 
-//                    if (link[0] != currentImageSpan && currentImageSpan != null) {
-//
-//                        MotionEvent cancelEvent = MotionEvent.obtain(event);
-//                        cancelEvent.setAction(MotionEvent.ACTION_CANCEL);
-//                        currentImageSpan.onTouchEvent(cancelEvent, widget);
-//                        currentImageSpan = null;
-//                    }
-                    link[0].onTouchEvent(event, widget);
+        TouchableSpan[] link = {};
 
-                } else if (action == MotionEvent.ACTION_DOWN) {
+        if (event.getY() > textTopBound && event.getY() < textBottomBound) {
+            link = buffer.getSpans(off, off, TouchableSpan.class);
+        }
 
-                    link[0].onTouchEvent(event, widget);
-//                    currentImageSpan = link[0];
+        Log.d("motion", "onTouchEvent(), touch event = " + MotionEventHelper.actionToString(event.getAction())
+                + ", coords = [" + event.getX() + ", " + event.getY() + "], is link = " + (link.length > 0));
+
+        if (link.length != 0) {
+
+            if (action == MotionEvent.ACTION_UP) {
+
+                if (pressedTouchableSpan != null) {
+
+                    if (link[0] != pressedTouchableSpan) {
+                        onCancelEvent(event, widget);
+                    } else {
+                        pressedTouchableSpan.onTouchEvent(event, widget);
+                        pressedTouchableSpan = null;
+                    }
                 }
 
-                return true;
-            } else {
+            } else if (action == MotionEvent.ACTION_DOWN) {
 
-//                if (currentImageSpan != null) {
-//                    currentImageSpan.onTouchEvent(event, widget);
-//                    currentImageSpan = null;
-//                }
+                if (pressedTouchableSpan != null) {
+                    onCancelEvent(event, widget);
+                }
+                link[0].onTouchEvent(event, widget);
+                pressedTouchableSpan = link[0];
+
+            } else if (action == MotionEvent.ACTION_MOVE) {
+
+                if (pressedTouchableSpan != null && pressedTouchableSpan != link[0]) {
+                    onCancelEvent(event, widget);
+                }
+
+            } else if (action == MotionEvent.ACTION_CANCEL) {
+
+                if (pressedTouchableSpan != null) {
+                    onCancelEvent(event, widget);
+                }
             }
+
+            return true;
+        }
+
+        if (pressedTouchableSpan != null) {
+
+            onCancelEvent(event, widget);
+            return true;
         }
 
         return super.onTouchEvent(widget, buffer, event);
     }
 
-//    private InteractiveImageSpan currentImageSpan;
-//    @Override
-//    protected boolean up(TextView widget, Spannable buffer) {
-//        Log.d("test" , "up, buffer = " + buffer.toString());
-//        if (action(UP, widget, buffer)) {
-//            return true;
-//        }
-//
-//        return super.up(widget, buffer);
-//    }
-//
-//    @Override
-//    protected boolean down(TextView widget, Spannable buffer) {
-//        Log.d("test" , "down, buffer = " + buffer.toString());
-//        if (action(DOWN, widget, buffer)) {
-//            return true;
-//        }
-//
-//        return super.down(widget, buffer);
-//    }
-//
-//    @Override
-//    protected boolean left(TextView widget, Spannable buffer) {
-//        Log.d("test" , "left, buffer = " + buffer.toString());
-//        if (action(UP, widget, buffer)) {
-//            return true;
-//        }
-//
-//        return super.left(widget, buffer);
-//    }
-//
-//    @Override
-//    protected boolean right(TextView widget, Spannable buffer) {
-//        Log.d("test" , "right, buffer = " + buffer.toString());
-//        if (action(DOWN, widget, buffer)) {
-//            return true;
-//        }
-//
-//        return super.right(widget, buffer);
-//    }
-//
-//    private boolean action(int what, TextView widget, Spannable buffer) {
-//
-//        Log.d("test" , "action, buffer = " + buffer.toString());
-//
-//        Layout layout = widget.getLayout();
-//
-//        int padding = widget.getTotalPaddingTop() +
-//                widget.getTotalPaddingBottom();
-//        int areatop = widget.getScrollY();
-//        int areabot = areatop + widget.getHeight() - padding;
-//
-//        int linetop = layout.getLineForVertical(areatop);
-//        int linebot = layout.getLineForVertical(areabot);
-//
-//        int first = layout.getLineStart(linetop);
-//        int last = layout.getLineEnd(linebot);
-//
-//        ClickableSpan[] candidates = buffer.getSpans(first, last, ClickableSpan.class);
-//
-//        int a = Selection.getSelectionStart(buffer);
-//        int b = Selection.getSelectionEnd(buffer);
-//
-//        int selStart = Math.min(a, b);
-//        int selEnd = Math.max(a, b);
-//
-//        if (selStart < 0) {
-//            if (buffer.getSpanStart(FROM_BELOW) >= 0) {
-//                selStart = selEnd = buffer.length();
-//            }
-//        }
-//
-//        if (selStart > last)
-//            selStart = selEnd = Integer.MAX_VALUE;
-//        if (selEnd < first)
-//            selStart = selEnd = -1;
-//
-//        switch (what) {
-//            case CLICK:
-//                if (selStart == selEnd) {
-//                    return false;
-//                }
-//
-//                ClickableSpan[] link = buffer.getSpans(selStart, selEnd, ClickableSpan.class);
-//
-//                if (link.length != 1)
-//                    return false;
-//
-//                link[0].onClick(widget);
-//                break;
-//
-//            case UP:
-//                int beststart, bestend;
-//
-//                beststart = -1;
-//                bestend = -1;
-//
-//                for (int i = 0; i < candidates.length; i++) {
-//                    int end = buffer.getSpanEnd(candidates[i]);
-//
-//                    if (end < selEnd || selStart == selEnd) {
-//                        if (end > bestend) {
-//                            beststart = buffer.getSpanStart(candidates[i]);
-//                            bestend = end;
-//                        }
-//                    }
-//                }
-//
-//                if (beststart >= 0) {
-//                    Selection.setSelection(buffer, bestend, beststart);
-//                    return true;
-//                }
-//
-//                break;
-//
-//            case DOWN:
-//                beststart = Integer.MAX_VALUE;
-//                bestend = Integer.MAX_VALUE;
-//
-//                for (int i = 0; i < candidates.length; i++) {
-//                    int start = buffer.getSpanStart(candidates[i]);
-//
-//                    if (start > selStart || selStart == selEnd) {
-//                        if (start < beststart) {
-//                            beststart = start;
-//                            bestend = buffer.getSpanEnd(candidates[i]);
-//                        }
-//                    }
-//                }
-//
-//                if (bestend < Integer.MAX_VALUE) {
-//                    Selection.setSelection(buffer, beststart, bestend);
-//                    return true;
-//                }
-//
-//                break;
-//        }
-//
-//        return false;
-//    }
+    private void onCancelEvent(MotionEvent event, TextView widget) {
+
+        MotionEvent cancelEvent = MotionEvent.obtain(event);
+        cancelEvent.setAction(MotionEvent.ACTION_CANCEL);
+        pressedTouchableSpan.onTouchEvent(cancelEvent, widget);
+        pressedTouchableSpan = null;
+    }
+
+    private TouchableSpan pressedTouchableSpan;
 }
